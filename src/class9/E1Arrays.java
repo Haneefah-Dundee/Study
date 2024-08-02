6b7c3ed771bb3cf5cad4eeb990a2406c6b387975
haneefah Dundee <haneefah.k.dundee@gmail.com>
2024-08-02 00:12:28 -0400

    2nd Commit "java Revision.Class 9-11"

--- src/class9/E1Arrays.java
+++ src/class9/E1Arrays.java
@@ -3,12 +3,15 @@ package class9;
 public class E1Arrays {
     public static void main(String[] args) {
 
-        String name="Jack";
+        String name="Jack";//declare and initialize
         String name2="Tom";
         String name3="Anna";
+        String name4; //declare without initializing
 //not feasible to store multiple names like this so we need array
 
-        String [] names={};
+        String [] names={"Jacob","Matt","Hanson"};
+        System.out.println(names[0]);
+        System.out.println(names[2]);
 
     }
 }
